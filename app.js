2218cb056f25929d79b8cdfaaa52b17270a00d79
syegod <ovisochin362@gmail.com>
2023-10-24 15:25:18 +0200

    prod branch start

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import { UserController } from './controllers/index.js';
-import {authCheck} from './utils/authCheck.js';
+import { authCheck } from './utils/authCheck.js';
 import { config } from 'dotenv';
+
 config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/api/auth/get-me', authCheck, UserController.getme);
+app.post('/api/auth/register', UserController.register);
+app.post('/api/auth/login', UserController.login);
+app.post('/api/add-to-list', authCheck, UserController.addtolist);
+app.post('/api/remove-from-list', authCheck, UserController.removefromlist);
+app.post('/api/change-rating', authCheck, UserController.changerating);
+app.post('/api/change-episodes', authCheck, UserController.changeepisodes);
 
-app.get('/auth/get-me', authCheck, UserController.getme);
-
-app.post('/auth/register', UserController.register);
-app.post('/auth/login', UserController.login);
-app.post('/add-to-list', authCheck, UserController.addtolist);
-app.post('/remove-from-list', authCheck, UserController.removefromlist);
-app.post('/change-rating', authCheck, UserController.changerating);
-app.post('/change-episodes', authCheck, UserController.changeepisodes);
-
-app.listen(process.env.HOST || 8000, () => {
-    console.log('Server is started...');
-});
+export default app;
